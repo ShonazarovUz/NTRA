70c108d34d639e4aac9304b5091707f2e9070697
STORM2609 <nasriddinshonazarov32@gmail.com>
2024-08-29 12:17:30 +0500

    NEW VERSION

--- controllers/create-ad.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-$branches = (new \App\Branch())->getBranches();
-
-
-loadView('admin/create-ad', ['branchlar' => $branches]);
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use App\Router;
-// use Controller\LoginAd;
 Router::get('/', fn()=> loadController('home'));
 Router::get('/branches', fn()=> loadController('branch'));
 
@@ -12,23 +11,23 @@ Router::get('/ads/{id}', function (int $id) {
 });
 
 Router::get('/ads/create', fn()=> loadController('create-ad'));
-
-Router::get('/status/create', fn()=> loadView('admin/createStatus'));
-
-Router::get('/Branch/create', fn()=> loadView('admin/createBranch'));
-
-Router::get('/register', fn()=> loadView('register'));
-Router::get('/login', fn()=> loadView('login'));
-
-
-
-
-
 Router::post('/ads/create', fn()=> loadController('createAd'));
 
+
+Router::get('/status/create', fn()=> loadView('admin/createStatus'));
 Router::post('/status/create', fn()=> loadController('create-status'));
 
+
+Router::get('/Branch/create', fn()=> loadView('admin/createBranch'));
 Router::post('/Branch/create', fn()=> loadController('create-branch'));
+
+
+Router::get('/register', fn()=> loadView('register'));
 Router::post('/register', fn()=> loadController('registerAd'));
+
+
+Router::get('/login', fn()=> loadView('login'));
 Router::post('/login', fn() => (new \Controller\LoginAd())->login());
+
+
 Router::errorResponse(404, 'Not Found');
